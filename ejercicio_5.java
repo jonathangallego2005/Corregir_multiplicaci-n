0b69bf846eb1e0e70fa347b6d2ea73fdb2ff9da4
jonathangallego2005 <1.4.0.jonathan@gmail.com>
2024-10-03 16:08:10 -0500

    Add files via upload

---
+++ ejercicio_5.java
@@ -0,0 +1,25 @@
+// public class Multiply {
+//     public static Double multiply(Double a, Double b) {
+//         return a * b
+//     }
+// este codigo se puede hacer de una forma correcta y muy basica: 
+import java.util.Scanner;
+public class ejercicio_5 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        int a,b,multiplicación;
+
+        System.out.print("Ingresa un número: ");
+        a = entrada.nextInt();
+        System.out.print("Ingresa otro número: ");
+        b = entrada.nextInt();
+
+        multiplicación = a * b;
+
+        System.out.println("La multiplicación de los números es: "+multiplicación);
+
+
+
+
+    }   
+}
